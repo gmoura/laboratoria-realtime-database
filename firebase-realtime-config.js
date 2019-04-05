f43a22e3785df77818419f3b6731a8ee49e3505f
Guilherme Moura <gmoura.nas@gmail.com>
2019-04-05 17:09:33 -0300

    remove firebase config

--- firebase-realtime-config.js
+++ firebase-realtime-config.js
@@ -1,9 +1,9 @@
 var config = {
-  apiKey: "AIzaSyDQ-DTxXM4K5NHv5F3W5JPd3ruQVai2YE8",
-  authDomain: "aulas-de-forro.firebaseapp.com",
-  databaseURL: "https://aulas-de-forro.firebaseio.com",
-  projectId: "aulas-de-forro",
-  storageBucket: "aulas-de-forro.appspot.com",
-  messagingSenderId: "740793149789"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
 };
 firebase.initializeApp(config);
